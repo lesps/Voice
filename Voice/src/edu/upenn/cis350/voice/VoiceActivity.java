8956506e20dd0aea680beea80a7f71671cafbabe
Spencer <Spencer@MrPoogles>
2012-03-20 20:52:28 -0400

    Initial commit.

---
+++ Voice/src/edu/upenn/cis350/voice/VoiceActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350.voice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VoiceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
