e817da03ff268cc5680f110282b7416afab76f84
Spencer <Spencer@MrPoogles>
2012-03-30 12:25:43 -0400

    Finished implementing switch case to switch between activities.

--- Voice/src/edu/upenn/cis350/voice/QuestionActivity.java
+++ Voice/src/edu/upenn/cis350/voice/QuestionActivity.java
@@ -9,6 +9,11 @@ import android.os.Bundle;
 public class QuestionActivity extends Activity {
 	
 	public static final int ACTIVITY_ButtonQuestion = 1;
+	public static final int ACTIVITY_SliderQuestion = 2;
+	public static final int ACTIVITY_DrawingQuestion = 3;
+	public static final int ACTIVITY_PictureQuestion = 4;
+	public static final int ACTIVITY_WheelQuestion = 5;
+	public static final int ACTIVITY_DragQuestion = 6;
 	
 	private ArrayList<Question> questionList;
 	private Question currentQuestion;
@@ -26,11 +31,31 @@ public class QuestionActivity extends Activity {
 	
 	public void switchQuestion(){
 		
+		Intent i;
 		switch(currentQuestion.getType()){
 		
 			case BUTTON:
-				Intent i = new Intent(this, ButtonQuestionActivity.class);
+				i = new Intent(this, ButtonQuestionActivity.class);
 				startActivityForResult(i, QuestionActivity.ACTIVITY_ButtonQuestion);
+				break;
+			case SLIDER:
+				i = new Intent(this, SliderQuestionActivity.class);
+				startActivityForResult(i, QuestionActivity.ACTIVITY_SliderQuestion);
+			case DRAG:
+				i = new Intent(this, DragQuestionActivity.class);
+				startActivityForResult(i, QuestionActivity.ACTIVITY_DragQuestion);
+				break;
+			case PICTURE:
+				i = new Intent(this, PictureQuestionActivity.class);
+				startActivityForResult(i, QuestionActivity.ACTIVITY_PictureQuestion);
+				break;
+			case WHEEL:
+				i = new Intent(this, WheelQuestionActivity.class);
+				startActivityForResult(i, QuestionActivity.ACTIVITY_WheelQuestion);
+				break;
+			case DRAWING:
+				i = new Intent(this, DrawingQuestionActivity.class);
+				startActivityForResult(i, QuestionActivity.ACTIVITY_DrawingQuestion);
 				break;
 		}
 	}
